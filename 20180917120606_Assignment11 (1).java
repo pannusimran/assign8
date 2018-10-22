fd13b4346f53feb9fadc95d1e77929f00ec2b86b
pannusimran <39096234+pannusimran@users.noreply.github.com>
2018-10-22 15:54:34 -0400

    Add files via upload

---
+++ 20180917120606_Assignment11 (1).java
@@ -0,0 +1,27 @@
+
+package assignment11;
+import java.util.Scanner;
+
+
+public class Assignment11 { 
+
+   
+    public static void main(String[] args) {
+        int n;
+        Scanner console = new Scanner(System.in);
+            System.out.print("How many lines " );
+            n=console.nextInt();
+     int i,j;
+     for (i=0;i<n;i++){
+        
+     
+         for(j=0;j<=i;j++)  {
+    
+     
+     System.out.print(" * ");
+    
+     }
+     System.out.println();
+   
+}
+}}
